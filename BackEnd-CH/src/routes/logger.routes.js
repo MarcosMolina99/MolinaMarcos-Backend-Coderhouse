a1269ae2e3028917bede3f67ae94062b6db73ac3
MarcosMolina99 <105325151+MarcosMolina99@users.noreply.github.com>
2023-10-12 19:35:20 -0300

    Delete BackEnd-CH/src/routes/logger.routes.js

--- BackEnd-CH/src/routes/logger.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getLogger } from "../controller/logger.controller.js";
-
-const routerLogger = Router()
-routerLogger.get("/", getLogger)
-
-export {routerLogger}
